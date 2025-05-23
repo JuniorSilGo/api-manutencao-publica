c8de43d6a41f5c64e11fabbd37c67bf6e8abe326
Rafael Moura <cfhmoura18@gmail.com>
2025-05-23 07:26:14 -0700

    Subindo primeiro commit inicializado

--- rogerio/src/app.module.ts
+++ rogerio/src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { SequelizeModule } from '@nestjs/sequelize';
+import { DBconfig } from './database/sequelize.config';
+
+import { Rotas } from './routes.module';
+
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot(DBconfig),
+    ...Rotas, // modulos de entidades
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ rogerio/src/routes.module.ts
@@ -0,0 +1,7 @@
+import { FuncionarioModule } from './funcionario/funcionario.module';
+// futuramente: import { ServicoModule } from './servico/servico.module';
+
+export const Rotas = [
+  FuncionarioModule,
+  // ServicoModule,
+];
